c2128be21b0f04fe45034b1534527a3c204d04ea
lpxxn <mi_duo@live.com>
2024-05-30 22:43:37 +0800

    feat: stop ble

--- py-robot/lib/stop_ble2.py
+++ py-robot/lib/stop_ble2.py
@@ -9,7 +9,11 @@ class BLE:
         self.timer = Timer(-1)
 
     def start_advertising(self):
-        self.ble.gap_advertise(100)
+        name = bytes('HELLOBLE', 'UTF-8')
+        # 以指定的时间间隔（以微秒为单位）开始广播
+        # 文档地址
+        # https://docs.micropython.org/en/latest/library/bluetooth.html?highlight=irq#broadcaster-role-advertiser
+        self.ble.gap_advertise(100, bytearray(b'\x02\x01\x02') + bytearray((len(name) + 1, 0x09)) + name)
         print("Advertising started")
         # Stop advertising after 30 seconds
         self.timer.init(period=30000, mode=Timer.ONE_SHOT, callback=self.stop_advertising)
