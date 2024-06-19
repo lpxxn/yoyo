b6c95f82692dab3e335aada8e7524962577c427e
lpxxn <mi_duo@live.com>
2024-06-19 09:59:29 +0800

    feat: send iot topic

--- clank/lib/pages/tabs/ito.dart
+++ clank/lib/pages/tabs/ito.dart
@@ -4,7 +4,23 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 
-void sendMsg() async {
+const endpoint =
+    'https://a244sdkbxgj2hu.ats.iot.cn-north-1.amazonaws.com.cn:8443';
+// The scanBleTopic to publish to and the message
+const scanBleTopic = 'testDeviceType/msg/scanBle';
+const unlockScreenTopic = 'testDeviceType/msg/unlockScreen';
+
+enum IotTopic { scanBleTopic, unlockScreenTopic }
+
+String getEndpoint(String endpoint, IotTopic topic) {
+  var selectedTopic = scanBleTopic;
+  if (topic == IotTopic.unlockScreenTopic) {
+    selectedTopic = unlockScreenTopic;
+  }
+  return '$endpoint/topics/${selectedTopic}?qos=1';
+}
+
+void sendMsg(String endpoint, IotTopic topic, Map<String, String> message) async {
   final dio = Dio();
 
   dio.httpClientAdapter = IOHttpClientAdapter()
@@ -21,15 +37,14 @@ void sendMsg() async {
       return HttpClient(context: context);
     };
 
-  // The topic to publish to and the message
-  const topic = 'testDeviceType/common';
-  final message = {
-    'message': 'Hello, world',
-  };
+  // final message = {
+  //   'message': 'Hello, world',
+  // };
 
   // The endpoint URL
-  const endpoint =
-      'https://a244sdkbxgj2hu.ats.iot.cn-north-1.amazonaws.com.cn:8443/topics/$topic?qos=1';
+
+  // const endpoint =
+  //     'https://a244sdkbxgj2hu.ats.iot.cn-north-1.amazonaws.com.cn:8443/topics/$scanBleTopic?qos=1';
 
   try {
     // Send the POST request with the message
